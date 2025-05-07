68ae1d371f0caabcf722a410268bded2ab079a1b
icyzeroice <ziv3@outlook.com>
2025-05-07 10:24:32 +0800

    feat: tab bar at bottom

--- config/appearance.lua
+++ config/appearance.lua
@@ -37,6 +37,7 @@ return {
    tab_max_width = 25,
    show_tab_index_in_tab_bar = false,
    switch_to_last_active_tab_when_closing_tab = true,
+   tab_bar_at_bottom = true,
 
    -- window
    window_background_opacity = 0.3,
