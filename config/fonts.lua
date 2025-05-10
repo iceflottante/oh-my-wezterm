69bab868430fb7eb270ac9bfbc6165b561db4f58
iceflottante <ziv3@outlook.com>
2025-05-10 19:22:48 +0800

    🔥 Remove unused code

--- config/fonts.lua
+++ config/fonts.lua
@@ -1,7 +1,6 @@
 local wezterm = require('wezterm')
 local platform = require('utils.platform')
 
--- local font = 'Maple Mono SC NF'
 local font_family = 'JetBrainsMono Nerd Font'
 local font_size = platform.is_mac and 12 or 9
 
